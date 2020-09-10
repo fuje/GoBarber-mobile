06a6c3f5aa5b5330bb407577026a0a997755cc60
Guilherme Fujiyoshi <gfuji1211@gmail.com>
2020-09-10 01:39:37 -0300

    Created input and button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container } from './styles';
+import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('on button press');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
